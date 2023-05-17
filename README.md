c793e04fc80fdd27ea2b38cb6e3839fc8b786954
aarzilli <alessandro.arzilli@gmail.com>
2023-05-17 10:53:56 +0200

    change to use -dumpdep from go 1.21

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ The Go linker will disable most deadcode elimination if it finds reachable calls
 Whydeadcode uses the call graph produced by the linker to display why `reflect.Value.MethodByName` or `reflect.Value.Method` are reachable. Use it like this:
 
 ```
-	go build -ldflags=-c your/package > call-graph.txt 2>&1
-	whydeadcode < call-graph.txt
+	go build -ldflags=-dumpdep your/package |& whydeadcode
 ```
+
+Needs Go 1.21 or later.
+
+Because of how `-dumpdep` works only the first result output by whydeadcode is real. Because of how `-dumpdep` works anything beyond the first result can be a false positive (i.e. things that look like they will affect deadcode elimination but won't if the first result is taken care of) and it can also have false negatives (i.e. things that will continue to keep deadcode elimination disabled if the first result is taken care of).
--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"os"
 	"bufio"
-	"strings"
 	"fmt"
-	"debug/gosym"
+	"os"
+	"strings"
 )
 
 func must(err error) {
@@ -15,69 +14,97 @@ func must(err error) {
 }
 
 var callers = map[string][]string{}
+var reflectMethods = map[string]bool{}
 
 func main() {
+	reflectMethods["reflect.Value.MethodByName"] = true
+	reflectMethods["reflect.Value.Method"] = true
+	seen := map[string]bool{}
+	first := true
+
 	buf := bufio.NewScanner(os.Stdin)
+bufScanLoop:
 	for buf.Scan() {
 		line := buf.Text()
-		fields := strings.Split(line, " calls ")
+		fields := strings.Split(line, " -> ")
 		if len(fields) != 2 {
 			continue
 		}
-		fields[0] = removeClosures(fields[0])
-		fields[1] = removeClosures(fields[1])
-		callers[fields[1]] = append(callers[fields[1]], fields[0])
-	}
-	
-	seen := map[string]bool{}
-	enum([]string{ "reflect.Value.MethodByName" }, seen)
-	enum([]string{ "reflect.Value.Method" }, seen)
-}
-
-func enum(path []string, seen map[string]bool) {
-	last := path[len(path)-1]
-	seen[last] = true
-	if len(callers[last]) == 0 {
-		if !isPublicMethod(last) {
-			fmt.Println(path[0], "called by:")
-			for i := 1; i < len(path); i++ {
-				fmt.Println("\t", path[i])
+		for i := range fields {
+			var flags string
+			fields[i], flags = splitFlags(fields[i])
+			if fields[i] == "" {
+				continue bufScanLoop
 			}
-			fmt.Println()
+			if strings.Contains(flags, "<ReflectMethod>") {
+				reflectMethods[fields[i]] = true
+			}
+		}
+		callers[fields[1]] = append(callers[fields[1]], fields[0])
+		if reflectMethods[fields[1]] && first {
+			first = false
+			enum([]string{fields[1]}, seen)
 		}
 	}
+
+	for reflectMethod := range reflectMethods {
+		enum([]string{reflectMethod}, seen)
+	}
+}
+
+func enum(path []string, seen map[string]bool) bool {
+	last := path[len(path)-1]
+	if last == "type:reflect.Value" || last == "type:*reflect.rtype" || last == "type:*reflect.Value" {
+		// these are almost always false positives so we skip them
+		return false
+	}
+	seen[last] = true
+	defer func() {
+		if !visitOnce(last) {
+			delete(seen, last)
+		}
+	}()
+	if len(callers[last]) == 0 {
+		if last != "_" {
+			return false
+		}
+		fmt.Println(path[0], "reachable from:")
+		for i := 1; i < len(path); i++ {
+			fmt.Println("\t", path[i])
+		}
+		fmt.Println()
+		return true
+	}
+	r := false
 	for _, next := range callers[last] {
 		if seen[next] {
 			continue
 		}
-		enum(append(path, next), seen)
-	}
-}
-
-func removeClosures(p string) string {
-	dot := strings.LastIndex(p, ".")
-	if dot < 0 {
-		return p
-	}
-	if !strings.HasPrefix(p[dot+1:], "func") {
-		return p
-	}
-	for i := dot+5; i < len(p); i++ {
-		if p[i] < '0' || p[i] > '9' {
-			return p
+		r2 := enum(append(path, next), seen)
+		r = r || r2
+		if r && visitOnce(last) {
+			return r
 		}
 	}
-	return p[:dot]
+	return r
 }
 
-func isPublicMethod(p string) bool {
-	sym := &gosym.Sym{ Name: p }
-	if sym.ReceiverName() == "" {
+func splitFlags(sym string) (string, string) {
+	for i := len(sym) - 1; i >= 0; i-- {
+		if sym[i] == ' ' {
+			return sym[:i], sym[i+1:]
+		}
+		if (sym[i] < 'a' || sym[i] > 'z') && (sym[i] < 'A' || sym[i] > 'Z') && (sym[i] != '<') && (sym[i] != '>') {
+			return sym, ""
+		}
+	}
+	return sym, ""
+}
+
+func visitOnce(sym string) bool {
+	slash := strings.Index(sym, "/")
+	if slash < 0 {
 		return false
 	}
-	bn := sym.BaseName()
-	if bn == "" {
-		return false
-	}
-	return bn[0] >= 'A' && bn[0] <= 'Z'
+	return strings.Index(sym[:slash], ".") >= 0
 }
